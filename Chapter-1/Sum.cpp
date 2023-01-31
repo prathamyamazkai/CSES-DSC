8e67ac9c4c2d83f34178dd49bc8d50eb888331c6
pratham <prathamyamazaki@gmail.com>
2023-01-31 23:35:29 +0530

    Sum Solved

---
+++ Chapter-1/Sum.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    long long a , b ;
+    cin >> a >> b ;
+
+    cout << a+b <<"\n";
+
+    return 0;
+
+}
